3d880ae46c93ba3d79a370fbe1cbd80116ccfe97
Steve Marks <info@biostall.com>
2013-02-28 10:02:29 +0000

    add ability to pass in an API key

--- application/libraries/Googlemaps.php
+++ application/libraries/Googlemaps.php
@@ -20,6 +20,7 @@ class Googlemaps {
 	var $adsenseFormat				= 'HALF_BANNER';			// The format of the AdUnit
 	var $adsensePosition			= 'TOP_CENTER';				// The position of the AdUnit
 	var $adsensePublisherID			= '';						// Your Google AdSense publisher ID
+	var $apiKey						= ''; 						// If you've got an API key you can use it by passing this parameter. Setup an API key here: https://code.google.com/apis/console
 	var $backgroundColor			= '';						// A hex color value shown as the map background when tiles have not yet loaded as the user pans
 	var $bicyclingOverlay			= FALSE;					// If set to TRUE will overlay bicycling information (ie. bike paths and suggested routes) onto the map by default 
 	var $center						= "37.4419, -122.1419";		// Sets the default center location (lat/long co-ordinate or address) of the map. If defaulting to the users location set to "auto"
@@ -1078,8 +1079,17 @@ class Googlemaps {
 		$this->output_js_contents = '';
 		$this->output_html = '';
 		
-		if ($this->https) { $apiLocation = 'https://maps-api-ssl'; }else{ $apiLocation = 'http://maps'; }
-		$apiLocation .= '.google.com/maps/api/js?sensor='.$this->sensor;
+		if ($this->apiKey!="") 
+		{
+			if ($this->https) { $apiLocation = 'https'; }else{ $apiLocation = 'http'; }
+			$apiLocation .= '://maps.googleapis.com/maps/api/js?key='.$this->apiKey.'&';
+		}
+		else
+		{
+			if ($this->https) { $apiLocation = 'https://maps-api-ssl'; }else{ $apiLocation = 'http://maps'; }
+			$apiLocation .= '.google.com/maps/api/js?';
+		}
+		$apiLocation .= 'sensor='.$this->sensor;
 		if ($this->region!="" && strlen($this->region)==2) { $apiLocation .= '&region='.strtoupper($this->region); }
 		if ($this->language!="") { $apiLocation .= '&language='.$this->language; }
 		$libraries = array();
